1952964da87abb01b8dec220fcd01b8562c07b4f
kogayushi <youshe.old.pleasure@gmail.com>
2018-04-21 23:33:57 +0900

    fix client

--- cloud-client/src/main/java/com/example/cloud_client/CloudClientApplication.java
+++ cloud-client/src/main/java/com/example/cloud_client/CloudClientApplication.java
@@ -3,20 +3,10 @@ package com.example.cloud_client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController
 @SpringBootApplication
 public class CloudClientApplication {
-
-    private final CloudClientProperties cloudClientProperties;
-
-    @RequestMapping("/info")
-    public String greeting() {
-        return cloudClientProperties.getFoo();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(CloudClientApplication.class, args);
---
+++ cloud-client/src/main/java/com/example/cloud_client/CloudClientRestController.java
@@ -0,0 +1,21 @@
+package com.example.cloud_client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CloudClientRestController {
+
+    private final CloudClientProperties cloudClientProperties;
+
+    @ResponseBody
+    @GetMapping(path = "/foo", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String foo() {
+        return cloudClientProperties.getFoo();
+    }
+
+}
