8af3f4084b9f40f83ef304c58f1f80702a777ec4
kogayushi <youshe.old.pleasure@gmail.com>
2018-05-03 16:41:33 +0900

    update something

--- cloud-client/src/main/java/com/example/cloud_client/CloudClientApplication.java
+++ cloud-client/src/main/java/com/example/cloud_client/CloudClientApplication.java
@@ -1,10 +1,8 @@
 package com.example.cloud_client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RequiredArgsConstructor
 @SpringBootApplication
 public class CloudClientApplication {
 
--- cloud-client/src/main/java/com/example/cloud_client/CloudClientProperties.java
+++ cloud-client/src/main/java/com/example/cloud_client/CloudClientProperties.java
@@ -3,12 +3,17 @@ package com.example.cloud_client;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
+
+@Validated
 @Data
 @Component
 @ConfigurationProperties(prefix = "info")
 public class CloudClientProperties {
 
+    @NotNull
     private String foo;
 
     private String local;
