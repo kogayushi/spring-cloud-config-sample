a78ac974a15a64a181f33c8e210766c5529f32d4
kogayushi <youshe.old.pleasure@gmail.com>
2018-05-04 23:03:59 +0900

    read logback.xml from cloud server

--- cloud-server/src/main/java/com/example/cloud_server/CloudServerApplication.java
+++ cloud-server/src/main/java/com/example/cloud_server/CloudServerApplication.java
@@ -3,12 +3,23 @@ package com.example.cloud_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableConfigServer
 @SpringBootApplication
-public class CloudServerApplication {
+public class CloudServerApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudServerApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+            .antMatchers("/**/logback*.xml").permitAll()
+            .anyRequest().authenticated().and()
+            .httpBasic().and()
+            .csrf().disable();
     }
 }
